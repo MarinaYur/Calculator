f8c292fb06d820dcdbc864d919bd37712def151d
Marina Yurkevich <marina.yur@Eugenes-MacBook-Pro.local>
2020-10-10 20:26:25 +0300

    feat: add digit output and operation plus

--- script.js
+++ script.js
@@ -8,9 +8,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const previousOperand = document.querySelector('.previous-operand');
     const currentOperand = document.querySelector('.current-operand');
-    const number = document.querySelectorAll('.number');
+    const numbers = document.querySelectorAll('.number');
+    const operations = document.querySelectorAll('.operation, .operation-equally');
+    const currentOperandValue = currentOperand.innerText;
+    const previousOperandValue = previousOperand.innerText;
+    // mathCalculations ();
 
-    document.querySelector('.calculator-grid').addEventListener('click', (e) => {
-        console.log(e.target);
-    })
+
+    numbers.forEach((number) => number.addEventListener('click', (e) => {
+        currentOperand.innerText += e.target.innerText;
+        console.log(typeof currentOperand.innerText);
+
+    }))
+    operations.forEach((operation) => operation.addEventListener('click', (e) => {
+        if (currentOperand.innerText !== "") {
+            if(previousOperand.innerText == "") {
+        previousOperand.innerText += Number(currentOperand.innerText) + e.target.innerText;
+            } else {
+                // console.log(operation);
+                // console.log(typeof operation);
+                mathCalculations (operation, e);
+            }
+        } else return;
+        currentOperand.innerText = '';
+
+        
+        // console.log(typeof currentOperand.innerText);
+    }))
+
+    function mathCalculations (a, e) {
+        switch (a.innerText) {
+            case '+': previousOperand.innerText = parseInt(previousOperand.innerText) + Number(currentOperand.innerText) + e.target.innerText;
+            break;
+        }
+    }
+
+
+
 });
