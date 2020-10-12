b84ed49cb19ef4f8001a768f5a2bc109f49bf55f
MarinaYur <marina.yurkev@gmail.com>
2020-10-12 12:12:17 +0300

    feat: add possibility of calculate negative numbers

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const operations = document.querySelectorAll('.operation, .operation-equally');
     const clearAll = document.querySelector('.clear-all');
     const deleteChar = document.querySelector('.delete-character');
-
+    let ideAquals = 0;
     numbers.forEach((number) => number.addEventListener('click', (e) => {
-        if (currentOperand.innerText == '' && number.innerText == '.') return;
         if (currentOperand.innerText.includes('.') && number.innerText == '.') return;
-        if (currentOperand.innerText[0] = ' ') {
-            currentOperand.innerText = e.target.innerText;
+        if (ideAquals && currentOperand.innerText !== '-') {
+            currentOperand.innerText = '';
+            currentOperand.innerText += e.target.innerText;
+            ideAquals = 0;
             return;
         }
         currentOperand.innerText += e.target.innerText;
@@ -19,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     operations.forEach((operation) => operation.addEventListener('click', (e) => {   
         let mathSign = previousOperand.innerText[previousOperand.innerText.length - 1];  
-        if (currentOperand.innerText !== "") {
+        if (currentOperand.innerText !== "" && currentOperand.innerText !== "-") {
             if (previousOperand.innerText == "") {
                 if (operation.innerText !== '=') {
                      previousOperand.innerText = Number(currentOperand.innerText) + e.target.innerText;
@@ -28,13 +29,18 @@ document.addEventListener("DOMContentLoaded", () => {
             } else {                
                 mathCalculations (mathSign, e);
             }              
-        } else return;
+        } else if (operation.innerText == '-' && !currentOperand.innerText.includes('-')) {
+            currentOperand.innerText += '-';
+        }
+        
+        
+        else return;
     }))
 
     function mathCalculations (a, event) {
         let previousOperandValue = parseFloat(previousOperand.innerText)
         switch (a) {
-            case '+':  previousOperand.innerText = previousOperandValue + Number(currentOperand.innerText);
+            case '+': previousOperand.innerText = previousOperandValue + Number(currentOperand.innerText);
                 break;
             case '-': previousOperand.innerText = previousOperandValue - Number(currentOperand.innerText);
                 break;
@@ -44,12 +50,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
         }
         if (event.target.innerText == '=') {
-                currentOperand.innerText = ' ' + previousOperand.innerText;
-                previousOperand.innerText = '';
-                return;                   
-            } else {
-                previousOperand.innerText += event.target.innerText;
-                currentOperand.innerText = '';
+            ideAquals = 1;
+            currentOperand.innerText = +(Number(previousOperand.innerText)).toFixed(2);
+            previousOperand.innerText = '';
+            return;                   
+        } else {
+            previousOperand.innerText += event.target.innerText;
+            currentOperand.innerText = '';
             }
     }
     // clear all digits using button AC
