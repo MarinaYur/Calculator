138b9105e234b2309aebc104d3a918ca4c18437d
Marina Yurkevich <marina.yurkev@gmail.com>
2020-10-13 12:06:07 +0300

    add code of exponentiation operation

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const previousOperand = document.querySelector('.previous-operand');
     const currentOperand = document.querySelector('.current-operand');
     const numbers = document.querySelectorAll('.number');
     const operations = document.querySelectorAll('.operation, .operation-equally');
     const clearAll = document.querySelector('.clear-all');
     const deleteChar = document.querySelector('.delete-character');
-    let ideAquals = 0;
+    const squareRoot = document.querySelector('.square-root');
+    const exponentiation = document.querySelector('.exponentiation');
+    let idAquals = 0;
     numbers.forEach((number) => number.addEventListener('click', (e) => {
+        if (currentOperand.innerText.includes('^')) {
+            currentOperand.innerText = parseFloat(currentOperand.innerText) ** e.target.innerText;
+            return;
+        }
         if (currentOperand.innerText.includes('.') && number.innerText == '.') return;
-        if (ideAquals && currentOperand.innerText !== '-') {
+        if (idAquals && currentOperand.innerText !== '-') {
             currentOperand.innerText = '';
             currentOperand.innerText += e.target.innerText;
-            ideAquals = 0;
+            idAquals = 0;
             return;
         }
         currentOperand.innerText += e.target.innerText;
     }))
-    
+    // simple mathematical operations
     operations.forEach((operation) => operation.addEventListener('click', (e) => {   
         let mathSign = previousOperand.innerText[previousOperand.innerText.length - 1];  
-        if (currentOperand.innerText !== "" && currentOperand.innerText !== "-") {
+        if (currentOperand.innerText !== "" && currentOperand.innerText !== ('-')) {
             if (previousOperand.innerText == "") {
                 if (operation.innerText !== '=') {
                      previousOperand.innerText = Number(currentOperand.innerText) + e.target.innerText;
@@ -31,12 +36,24 @@ document.addEventListener("DOMContentLoaded", () => {
             }              
         } else if (operation.innerText == '-' && !currentOperand.innerText.includes('-')) {
             currentOperand.innerText += '-';
-        }
-        
-        
-        else return;
+        } else return;
     }))
-
+    //square root operation 
+    squareRoot.addEventListener('click', () => {
+        if (currentOperand.innerText !== '' && currentOperand.innerText !== '-') {
+            if (currentOperand.innerText.includes('-')) {
+                alert('Error: it is impossible to extract the square root of a negative number');
+            } else {
+            currentOperand.innerText = Math.sqrt(currentOperand.innerText)
+            }
+        }
+    })
+    // exponentiation operation
+    exponentiation.addEventListener('click', () => {
+        if (currentOperand.innerText !== '' && currentOperand.innerText !== '-' && !currentOperand.innerText.includes('^')) {
+            currentOperand.innerText += '^';
+        }
+    })   
     function mathCalculations (a, event) {
         let previousOperandValue = parseFloat(previousOperand.innerText)
         switch (a) {
@@ -50,14 +67,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
         }
         if (event.target.innerText == '=') {
-            ideAquals = 1;
-            currentOperand.innerText = +(Number(previousOperand.innerText)).toFixed(2);
+            idAquals = 1;
+            currentOperand.innerText = +(Number(previousOperand.innerText)).toFixed(6);
             previousOperand.innerText = '';
             return;                   
-        } else if (event.target.innerText == '√'){
-            currentOperand.innerText = currentOperand.innerText + '√';
-        }
-        
+        } 
         else {
             previousOperand.innerText += event.target.innerText;
             currentOperand.innerText = '';
@@ -68,9 +82,8 @@ document.addEventListener("DOMContentLoaded", () => {
         currentOperand.innerText = '';
         previousOperand.innerText = '';
     })
-
     //delete one or some characters
     deleteChar.addEventListener('click', () => {
-        currentOperand.innerText = currentOperand.innerText.substr(0, currentOperand.innerText.length - 1);       
+        currentOperand.innerText = currentOperand.innerText.substring(0, currentOperand.innerText.length - 1);       
     })
 });
